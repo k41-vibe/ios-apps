27178117e748ebec22dd6133341b4d9d8e9c749b
unknown <ruto.isaka@gmail.com>
2026-09-11 14:01:04 +0000

    build: LCProbe

--- apps/LCProbe/postbuild.sh
+++ apps/LCProbe/postbuild.sh
@@ -8,13 +8,16 @@ mkdir -p "$FW"
 SDK=$(xcrun --sdk iphoneos --show-sdk-path)
 WORK=$(mktemp -d)
 N=800
+cat > "$WORK/gen.sh" <<EOF
+#!/bin/sh
+i=\$1
+src="$WORK/p\$i.c"
+printf 'int probe_%d(void){return %d;}\n' "\$i" "\$i" > "\$src"
+clang -target arm64-apple-ios16.0 -isysroot "$SDK" -dynamiclib -Os \\
+  -install_name "@rpath/libprobe\$i.dylib" -o "$FW/libprobe\$i.dylib" "\$src"
+EOF
+chmod +x "$WORK/gen.sh"
 echo "generating $N probe dylibs..."
-seq 1 $N | xargs -P 8 -I{} sh -c '
-  i={}
-  src="'"$WORK"'/p$i.c"
-  printf "int probe_%d(void){return %d;}\n" "$i" "$i" > "$src"
-  clang -target arm64-apple-ios16.0 -isysroot "'"$SDK"'" -dynamiclib -Os \
-    -install_name "@rpath/libprobe$i.dylib" -o "'"$FW"'/libprobe$i.dylib" "$src"
-'
-ls "$FW" | wc -l
+seq 1 $N | xargs -P 8 -n 1 "$WORK/gen.sh"
+echo "dylibs: $(ls "$FW" | wc -l)"
 du -sh "$FW"
